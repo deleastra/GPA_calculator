90eabc7b3d8431fb069894bba74103998abdd70c
Deleastra <blaziken49@gmail.com>
2019-01-06 17:52:10 +0700

    keep grades data in dict

--- gpa_calculator.py
+++ gpa_calculator.py
@@ -1,5 +1,6 @@
 #! -*- codding: utf-8 -*-
 import csv
+import re
 
 
 class GpaFile:
@@ -7,6 +8,23 @@ class GpaFile:
         self.gpa_filename = gpa_filename
         self.gpa_file = open(gpa_filename)
         self.reader = csv.reader(self.gpa_file)
+        self.gpa_data = {}
+        self.get_grades()
+
+    def get_grades(self):
+        for row in self.reader:
+            match_semester = re.match(r'ปีการศึกษา (\d+) ภาคการศึกษาที่ (\d+)', row[0], re.M)
+            if match_semester:
+                year = match_semester.group(1)
+                semester = match_semester.group(2)
+                self.create_grades_dict(year, semester)
+            elif row[0].isnumeric() and isinstance(row[1], str) and row[2].isnumeric() and isinstance(row[3], str):
+                self.gpa_data[year][semester].append([row[1], row[2], row[3]])
+
+    def create_grades_dict(self, year, semester):
+        if year not in self.gpa_data.keys():
+            self.gpa_data[year] = {}
+        self.gpa_data[year][semester] = []
 
 
 # filename = input("Enter Filename: ")
